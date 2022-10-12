bd7f936f8ea2198a3dc8911d064558d585932117
Maximvdw <Maximvdw@users.noreply.github.com>
2022-10-12 09:56:32 +0000

    deploy: b24109ec38e4172cda59bcfab30a7b780a829403

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,19 @@
 window.onscroll = function () { scrollFunction() };
 
-const thresshold = 220;
+const header_height = 330;
+const header_height_mobile = 230;
+const header_large_offset = 100;
+const header_height_scroll = 110;
+const header_height_scroll_mobile = 70;
+
 function scrollFunction() {
+  const isMobile = window.innerWidth < 1100;
+  // Header element
   const header = document.getElementsByTagName("header")[0];
+  // Header element has a different size for mobile and desktop
+  const thresshold = (isMobile ? header_height_mobile : header_height) -
+    (isMobile ? header_height_scroll_mobile : header_height_scroll) +
+    (header.classList.contains("large") ? header_large_offset : 0);
   const scrolled = document.body.scrollTop > thresshold || document.documentElement.scrollTop > thresshold;
   const scrollClass = header.classList.contains("scroll");
   if (scrolled && !scrollClass) {
