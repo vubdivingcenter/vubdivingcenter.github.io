12f4dce65dc5c72db07eef75ec2216150de0ebd6
Maximvdw <Maximvdw@users.noreply.github.com>
2025-09-14 11:26:30 +0000

    deploy: f7bce7ac05c24c35636372d109f7c47110b7c5cd

--- user/assets/status-tap-legacy.js
+++
@@ -1,5 +0,0 @@
-System.register(["./index-legacy.js"],(function(e,t){"use strict";var n,r,s,o,a;return{setters:[e=>{n=e.a3,r=e.a4,s=e.a5,o=e.a6,a=e.a7}],execute:function(){
-/*!
-             * (C) Ionic http://ionicframework.com - MIT License
-             */
-e("startStatusTap",(()=>{const e=window;e.addEventListener("statusTap",(()=>{n((()=>{const t=e.innerWidth,n=e.innerHeight,i=document.elementFromPoint(t/2,n/2);if(!i)return;const c=r(i);c&&new Promise((e=>s(c,e))).then((()=>{o((async()=>{c.style.setProperty("--overflow","hidden"),await a(c,300),c.style.removeProperty("--overflow")}))}))}))}))}))}}}));
