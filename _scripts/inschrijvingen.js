1d0668b0c81832bc768b0ed47df3e5a61304eefb
Maxim Van de Wynckel <maxim@mvdw-software.com>
2025-09-14 18:18:21 +0200

    ci: fixed inschrijvingen generator

--- _scripts/inschrijvingen.js
+++ _scripts/inschrijvingen.js
@@ -6,27 +6,40 @@ import path from 'path';
 import ejs from 'ejs';
 import puppeteer from 'puppeteer';
 import sgMail from '@sendgrid/mail';
+
 dotenv.config();
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
+const vdcDataPath = path.resolve(process.cwd(), '_data', 'vdc.json');
+const vdcData = JSON.parse(fs.readFileSync(vdcDataPath, 'utf8'));
+const lidkaartTemplatePath = path.resolve(process.cwd(), '_templates', 'ledenkaart.ejs');
+const lidkaartTemplate = fs.readFileSync(lidkaartTemplatePath, 'utf8');
+const outputDir = path.resolve(process.cwd(), '_output');
+if (!fs.existsSync(outputDir)) fs.mkdirSync(outputDir);
+
+/**
+ * Bereken het bedrag op basis van het type lidmaatschap
+ * @param {*} type 
+ * @param {*} vdc 
+ * @returns 
+ */
 function getBedrag(type, vdc) {
     switch (type) {
-        case 'Nieuw lid':
-            return vdc.lidgeld.lid + vdc.lidgeld.opleiding;
-        case 'Bestaand lid':
-            return vdc.lidgeld.lid;
-        case 'Steunend lid':
-            return vdc.lidgeld.steunend;
+        case 'Nieuw lid': return vdc.lidgeld.lid + vdc.lidgeld.opleiding;
+        case 'Bestaand lid': return vdc.lidgeld.lid;
+        case 'Steunend lid': return vdc.lidgeld.steunend;
+        default: return 0;
     }
 }
 
 /**
- * Send an email using SendGrid
+ * Verzend een e-mail met SendGrid
  * @param {*} to 
  * @param {*} subject 
  * @param {*} template 
  * @param {*} templateData 
  * @param {*} attachments 
+ * @returns 
  */
 function sendEmail(to, subject, template, templateData, attachments = []) {
     return new Promise((resolve, reject) => {
@@ -57,116 +70,112 @@ function sendEmail(to, subject, template, templateData, attachments = []) {
     });
 }
 
-// Load vdc data from /_data/vdc.json
-const vdcDataPath = path.resolve(process.cwd(), '_data', 'vdc.json');
-const vdcData = JSON.parse(fs.readFileSync(vdcDataPath, 'utf8'));
-
-const serviceAccountAuth = new JWT({
-    email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-    key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    scopes: ['https://www.googleapis.com/auth/spreadsheets'],
-});
-
-const doc = new GoogleSpreadsheet(process.env.GOOGLE_SPREADSHEET_ID, serviceAccountAuth);
-
-await doc.loadInfo();
-const sheet = doc.sheetsByIndex[0];
-
-// Get all rows
-let rows = await sheet.getRows();
-// Filter rows where 'Lidkaart verzonden' is not 'ja'
-rows = rows.filter(row => row.get('Lidkaart verzonden') !== 'ja');
-
-const lidkaartTemplatePath = path.resolve(process.cwd(), '_templates', 'ledenkaart.ejs');
-const lidkaartTemplate = fs.readFileSync(lidkaartTemplatePath, 'utf8');
-const outputDir = path.resolve(process.cwd(), '_output');
-if (!fs.existsSync(outputDir)) {
-    fs.mkdirSync(outputDir);
+/**
+ * Verstuur een betalingsverzoek
+ * @param {*} row 
+ * @param {*} vdcData 
+ */
+async function sendBetalingsverzoek(row, vdcData) {
+    const firstName = row.get('Voornaam');
+    const lastName = row.get('Achternaam');
+    const email = row.get('E-mail');
+    const type = row.get('Optie');
+    console.log(`Sending betalingsverzoek to ${firstName} ${lastName} <${email}>`);
+    await sendEmail(
+        email,
+        `Welkom bij het VUB Diving Center (${vdcData.lidjaar.start}-${vdcData.lidjaar.einde})`,
+        "email_betalingsverzoek",
+        { firstName, lastName, vdc: vdcData, type: `${type} (${getBedrag(type, vdcData)} euro)` },
+        []
+    );
+    row.set('Betalingsverzoek verzonden', 'ja');
+    if (type === 'Steunend lid') row.set('Lidkaart verzonden', 'ja');
+    await row.save();
+    console.log(`Betalingsverzoek sent to ${firstName} ${lastName}`);
 }
 
-// Generate HTML files
-for (const row of rows) {
+/**
+ * Genereer en verstuur de lidkaart
+ * @param {*} row 
+ * @param {*} vdcData 
+ */
+async function generateAndSendLidkaart(row, vdcData) {
     const firstName = row.get('Voornaam');
     const lastName = row.get('Achternaam');
     const email = row.get('E-mail');
     const firstNameSanitized = firstName.replace(/[^a-zA-Z0-9]/g, '');
     const lastNameSanitized = lastName.replace(/[^a-zA-Z0-9]/g, '');
-    const type = row.get('Optie');
+    const fileName = `VDC_${lastNameSanitized}${firstNameSanitized}_${vdcData.lidjaar.start}-${vdcData.lidjaar.einde}`;
+    console.log(`Generating lidkaart voor ${firstName} ${lastName} <${email}>`);
+    const html = ejs.render(lidkaartTemplate, {
+        row,
+        fileName,
+        start: vdcData.lidjaar.start,
+        einde: vdcData.lidjaar.einde,
+        voornaam: firstName.toUpperCase(),
+        achternaam: lastName.toUpperCase(),
+    });
+    const outputPath = path.join(outputDir, `${fileName}.html`);
+    fs.writeFileSync(outputPath, html, 'utf8');
+    const pdfPath = path.join(outputDir, `${fileName}.pdf`);
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setContent(html, { waitUntil: 'networkidle0' });
+    await page.pdf({ path: pdfPath, format: 'A4', printBackground: true });
+    await browser.close();
+    await sendEmail(
+        email,
+        `Uw VDC Lidkaart voor ${vdcData.lidjaar.start}-${vdcData.lidjaar.einde}`,
+        "email_ledenkaart",
+        { firstName, lastName, vdc: vdcData },
+        [{ path: pdfPath, filename: `${fileName}.pdf`, type: 'application/pdf' }]
+    );
+    row.set('Lidkaart verzonden', 'ja');
+    await row.save();
+    console.log(`Lidkaart voor ${firstName} ${lastName} opgeslagen als ${fileName}.html en ${fileName}.pdf`);
+}
 
-    // If Betalingsverzoek verzonden is not 'ja'
-    if (row.get('Betalingsverzoek verzonden') !== 'ja') {
-        console.log(`Sending betalingsverzoek to ${firstName} ${lastName} <${email}>`);
-
-        // Send email with instructions
-        sendEmail(
-            email,
-            `Welkom bij het VUB Diving Center (${vdcData.lidjaar.start}-${vdcData.lidjaar.einde})`,
-            "email_betalingsverzoek",
-            { firstName, lastName, vdc: vdcData, type: `${type} (${getBedrag(type, vdcData)} euro)` },
-            []
-        ).then(async () => {
-            // Mark row as 'Betalingsverzoek verzonden' = 'ja'
-            row.set('Betalingsverzoek verzonden', 'ja');
-            // Bij steunend lid, markeer ook lidkaart verzonden
-            if (type === 'Steunend lid') {
-                row.set('Lidkaart verzonden', 'ja');
-            }
+async function processInschrijvingen() {
+    const serviceAccountAuth = new JWT({
+        email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+        key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+        scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+    });
+    const doc = new GoogleSpreadsheet(process.env.GOOGLE_SPREADSHEET_ID, serviceAccountAuth);
+    await doc.loadInfo();
+    const sheet = doc.sheetsByIndex[0];
+    let rows = await sheet.getRows();
+    // Validate the secret code if 'paid' is set to true
+    for (const row of rows) {
+        const securityCode = row.get('CODE');
+        const betaald = row.get('Betaald');
+        if (securityCode !== process.env.REGISTRATION_CODE && betaald === 'ja') {
+            console.warn(`Invalid security code for ${row.get('Voornaam')} ${row.get('Achternaam')}, skipping...`);
+            row.set('Betaald', 'ONGELDIG');
             await row.save();
-            console.log(`Betalingsverzoek sent to ${firstName} ${lastName}`);
-        }).catch(err => {
-            console.error(`Failed to send betalingsverzoek to ${firstName} ${lastName}:`, err);
-        });
-    } else {
-        // If betaald is ja
-        if (row.get('Betaald') === 'ja') {
-            const fileName = `VDC_${lastNameSanitized}${firstNameSanitized}_${vdcData.lidjaar.start}-${vdcData.lidjaar.einde}`;
-            console.log(`Generating lidkaart voor ${firstName} ${lastName} <${email}>`);
+        } else {
+            console.log(`Valid security code for ${row.get('Voornaam')} ${row.get('Achternaam')}`);
+            row.set('CODE', process.env.REGISTRATION_CODE);
+            await row.save();
+        }
+    }
 
-            // Render HTML from EJS template
-            const html = ejs.render(lidkaartTemplate, {
-                row,
-                fileName,
-                start: vdcData.lidjaar.start,
-                einde: vdcData.lidjaar.einde,
-                voornaam: firstName.toUpperCase(),
-                achternaam: lastName.toUpperCase(),
-            });
-
-            // Save HTML to file
-            const outputPath = path.join(outputDir, `${fileName}.html`);
-            fs.writeFileSync(outputPath, html, 'utf8');
-
-            // Save HTML to PDF (A4)
-            const pdfPath = path.join(outputDir, `${fileName}.pdf`);
-            const browser = await puppeteer.launch();
-            const page = await browser.newPage();
-            await page.setContent(html, { waitUntil: 'networkidle0' });
-            await page.pdf({
-                path: pdfPath,
-                format: 'A4',
-                printBackground: true
-            });
-            await browser.close();
-
-            // Send email with PDF attachment
-            sendEmail(
-                email,
-                `Uw VDC Lidkaart voor ${vdcData.lidjaar.start}-${vdcData.lidjaar.einde}`,
-                "email_ledenkaart",
-                { firstName, lastName, vdc: vdcData },
-                [{
-                    path: pdfPath,
-                    filename: `${fileName}.pdf`,
-                    type: 'application/pdf'
-                }]
-            ).then(async () => {
-                // Mark row as 'Lidkaart verzonden' = 'ja'
-                row.set('Lidkaart verzonden', 'ja');
-                await row.save();
-                console.log(`Lidkaart voor ${firstName} ${lastName} opgeslagen als ${fileName}.html en ${fileName}.pdf`);
-            }).catch(err => {
-                console.error(`Failed to send ledenkaart to ${firstName} ${lastName}:`, err);
-            });
+    rows = rows.filter(row => row.get('Lidkaart verzonden') !== 'ja');
+    for (const row of rows) {
+        try {
+            if (row.get('Betalingsverzoek verzonden') !== 'ja') {
+                await sendBetalingsverzoek(row, vdcData);
+            } else if (row.get('Betaald') === 'ja') {
+                await generateAndSendLidkaart(row, vdcData);
+            }
+        } catch (err) {
+            const firstName = row.get('Voornaam');
+            const lastName = row.get('Achternaam');
+            console.error(`Error processing ${firstName} ${lastName}:`, err);
         }
     }
 }
+
+processInschrijvingen().catch(err => {
+    console.error('Fatal error:', err);
+});
